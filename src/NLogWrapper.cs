3a37db32881aace8c28ec866e6e8753943d5d931
npolyak <nick.polyak@verizon.net>
2022-02-21 16:41:14 -0500

    added a missing file

---
+++ src/NLogWrapper.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using NP.Utilities;
+using NP.Utilities.Attributes;
+
+namespace NP.NLogAdapter
+{
+    [Implements(typeof(ILog))]
+    public class NLogWrapper : ILog
+    {
+        public static NLogWrapper Instance { get; } = new NLogWrapper();
+
+        private ILogger _logger;
+
+        public NLogWrapper()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void Log(LogKind logKind, string component, string msg)
+        {
+            LogEventInfo logEventInfo = new LogEventInfo(logKind.ToLogLevel(), "DefaultLogger", $"{msg}");
+
+            logEventInfo.Properties.Add("Component", component);
+
+            _logger.Log(logEventInfo);
+        }
+
+        public static void SetLog()
+        {
+            NP.Utilities.Logger.SetLog(Instance);
+        }
+    }
+}
